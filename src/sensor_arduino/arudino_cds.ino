431ecf02f8837fe157acd3f86b7a75b3146d76d1
Suzuki22 <suzuki@lmd.ist.hokudai.ac.jp>
2018-10-20 18:11:20 +0900

    sensor code and server code updated by suzuki

---
+++ src/sensor_arduino/arudino_cds.ino
@@ -0,0 +1,31 @@
+int pin = 0; //センサーのピン番号
+int get_a0 = 0; //センサーデータ取得
+int flag = 0;
+int s=0;
+void setup(){
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+  //Serial.println("hello world");
+  get_a0 = analogRead(pin); // 照度センサーからデータを取得
+  Serial.println(get_a0);
+  s = 0;
+  Serial.println(s); // シリアルモニタに出力
+  if ( get_a0 > 400 ) {
+    if(flag == 1){
+      s = 2000;//ポストのフタOpen
+      Serial.println(s); // シリアルモニタに出力
+    }
+    flag=1;
+  } else if ( get_a0 <= 400 ) {
+    if(flag == 0){
+      s = 1000;//ポストのフタClose
+      Serial.println(s); // シリアルモニタに出力
+    }
+    flag=0;
+  }
+  delay(500);
+}
---
+++ src/server_raspberrypi/linepod.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env.python
+#/-*-/coding:/utf-8/-*-
+# socket server making
+
+import socket
+import requests
+import serial
+import time
+import os
+postFlg = 0
+
+def ArduinoSensing():
+    global postFlg
+    print('Light value sesing via CdS sensor ->start!')
+    print("CallTest" + str(postFlg))
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    time.sleep(2)
+    openFlg = 0
+	
+    while (postFlg != 1):
+        data = ser.readline()#Loading the data from Arduino
+        data2 = int(data)#Convert int
+	print('Light value:' + str(data2))
+	if data2 == 2000:
+		print('Status:Cover of post open')
+		time.sleep(1)
+		openFlg = 1
+	elif data2 == 1000:
+		print('Status:Cover of post closed')
+		time.sleep(1)
+		print(str(openFlg))
+		if openFlg == 1:
+                    postFlg = 1
+                    print('Huzai-Tsuuti submitted')
+    
+def LineNotify():
+    print('Notification to smart phone via LINE')
+    global postFlg
+    postFlg = 0
+    url = "https://notify-api.line.me/api/notify"
+    #token = "02mBhuc3Nx15jVDw2QcADvP6Vn6ehuZwZgsFogQUTEv"#Susumu's token
+    token = "kjiLoIkLRUQUHuq9HOGuuhJBMfs9M8LjdlF08oqNNEj"#Naritakun's token
+    headers = {"Authorization" : "Bearer "+ token}
+    message =  'FuzaiTsuuti'
+    payload = {"message" :  message}
+    r = requests.post(url ,headers = headers ,params=payload)
+    print("LINE")
+    print("LineNotify" + str(postFlg))
+    
+def main():
+    print('Server start')
+    # AF = IPv4
+    # In TCP/IP case, using SOCArduinoSensingK_STREAM
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # IPaddress and PORT
+    s.bind(('172.20.10.7', 50007))
+    # 1 Connection
+    s.listen(1)
+    # Waiting until connection
+    conn, addr = s.accept()
+    
+    if conn is None:
+        print('conn is None')
+    print('Connected by'+str(addr))
+    while True:
+        data = conn.recv(1024)
+        print('data: {}, addr: {}'.format(data, addr))
+        if data is not None:
+            #Data recieving from Rasberry Pi baesd osn NEC API
+            ArduinoSensing()
+
+        if postFlg == 1:
+            #Notification to smart phone via LINE
+            LineNotify()
+        
+if __name__== '__main__':
+    main()
+    
