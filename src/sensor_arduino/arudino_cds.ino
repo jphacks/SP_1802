1ec585f9232c90e1cb9c1af0b9ded249308abd9d
TABATA <koji@M-TabataYotaro.local>
2018-10-21 13:08:07 +0900

    update arduino_cds.ino

--- src/sensor_arduino/arudino_cds.ino
+++ src/sensor_arduino/arudino_cds.ino
@@ -12,20 +12,20 @@ void loop()
   //Serial.println("hello world");
   get_a0 = analogRead(pin); // 照度センサーからデータを取得
   Serial.println(get_a0);
-  s = 0;
-  Serial.println(s); // シリアルモニタに出力
-  if ( get_a0 > 400 ) {
+  //s = 0;
+  //Serial.println(s); // シリアルモニタに出力
+  if ( get_a0 > 100 ) {
     if(flag == 1){
       s = 2000;//ポストのフタOpen
       Serial.println(s); // シリアルモニタに出力
     }
     flag=1;
-  } else if ( get_a0 <= 400 ) {
+  } else if ( get_a0 <= 100 ) {
     if(flag == 0){
       s = 1000;//ポストのフタClose
       Serial.println(s); // シリアルモニタに出力
     }
     flag=0;
   }
-  delay(500);
+  delay(1000);
 }
